5d13279cc7d3dbdd1957ab0cd9ccde403e64ff00
com <x7788@naver.com>
2022-01-25 18:55:29 +0900

    이미지 배치 준비

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const playBtn = document.querySelector('.game-btn')
 const timer = document.querySelector('.time')
 const modal = document.querySelector('.modal')
+const field = document.querySelector('.game-field')
+const fieldRect = field.getBoundingClientRect()
+console.log(fieldRect)
 
 const playIcon = document.querySelector('.play-icon')
 let started = false // 게임의 상태를 기억하는 변수
@@ -20,12 +23,14 @@ playBtn.addEventListener('click', () => {
 
 function startGame() {
   startTimer()
+  onField()
 }
 
 function stopGame() {
   stopTimer()
 }
 
+// 타이머
 function startTimer() {
   playIcon.setAttribute('class', 'fas fa-stop')
 
@@ -36,10 +41,31 @@ function startTimer() {
     if (second === -1) {
       clearInterval(interval)
       modal.classList.remove('hidden')
+      playBtn.remove()
     }
   }, 1000)
 }
 
 function stopTimer() {
   clearInterval(interval)
+  modal.classList.remove('hidden')
+  playBtn.remove()
+}
+
+// 아이템 필드 위에 배치하기
+function onField() {
+  addItem('carrot', 6, 'img / carrot.png')
+  addItem('bug', 6, 'img / bug.png')
+}
+
+function addItem(name, count, imgPath) {
+  const x = fieldRect.width
+  const y = fieldRect.height
+  for (i = 0; i <= count; i++) {
+    const item = document.createElement('img')
+    item.setAttribute('class', name)
+    item.setAttribute('src', imgPath)
+    item.style.transform = `translate(${x}px, ${y}px)`
+    field.append('item')
+  }
 }
