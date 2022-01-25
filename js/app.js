85694a2f175a55b0a05c16fa4c26df2742a219cb
com <x7788@naver.com>
2022-01-25 15:07:16 +0900

    playBtn 클릭 시 icon 변화

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 const playBtn = document.querySelector('.game-btn')
 const timer = document.querySelector('.time')
+const modal = document.querySelector('.modal')
+
+const playIcon = document.querySelector('.play-icon')
 
 // 시작 버튼을 누르면 타이머가 동작 (10초)
 let second = 10
 
 function gamePlay() {
+  playIcon.setAttribute('class', 'fas fa-stop')
+
   let interval = setInterval(function () {
     timer.innerHTML = `${second}`
     second--
     if (second === -1) {
       clearInterval(interval)
+      modal.classList.remove('hidden')
     }
   }, 1000)
 }
