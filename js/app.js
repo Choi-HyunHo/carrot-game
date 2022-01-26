a6a6ebddf77de8888c9e96a6c893025803253681
com <x7788@naver.com>
2022-01-26 16:19:47 +0900

    아이템 배치 완성

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const timer = document.querySelector('.time')
 const modal = document.querySelector('.modal')
 const field = document.querySelector('.game-field')
 const fieldRect = field.getBoundingClientRect()
-console.log(fieldRect)
+const CARROT_SIZE = 80
 
 const playIcon = document.querySelector('.play-icon')
 let started = false // 게임의 상태를 기억하는 변수
@@ -54,25 +54,24 @@ function stopTimer() {
 
 // 아이템 필드 위에 배치하기
 function onField() {
-  addItem('carrot', 6, 'img / carrot.png')
-  addItem('bug', 6, 'img / bug.png')
+  addItem('carrot', 6, 'img/carrot.png')
+  addItem('bug', 6, 'img/bug.png')
 }
 
 function addItem(name, count, imgPath) {
   const x1 = 0 // 사각형의 시작점 (좌측 상단)
   const y1 = 0
-  const x2 = fieldRect.width // 사각형 우측 하단
-  const y2 = fieldRect.height
-  for (i = 0; i <= count; i++) {
+  const x2 = fieldRect.width - CARROT_SIZE // 사각형 우측 하단. 빼는 이유 - 아이템의 x,y 기준으로 포지션이 지정되기 때문에
+  const y2 = fieldRect.height - CARROT_SIZE
+  for (let i = 0; i < count; i++) {
     const item = document.createElement('img')
     item.setAttribute('class', name)
     item.setAttribute('src', imgPath)
     item.style.position = 'absolute'
     const horizontal = randomField(x1, x2)
     const vertical = randomField(y1, y2)
-    horizontal.style.transform = `translate(${x1}px, ${x2}px)`
-    vertical.style.transform = `translate(${y1}px, ${y2}px)`
-    field.append('item')
+    item.style.transform = `translate(${horizontal}px, ${vertical}px)`
+    field.append(item)
   }
 }
 
