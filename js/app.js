57c1b5645a8b7352d3cdad2eabb5c81d5842a50b
com <x7788@naver.com>
2022-01-26 17:35:01 +0900

    버튼 전환에 따른 함수 추가

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const playBtn = document.querySelector('.game-btn')
 const timer = document.querySelector('.time')
+const counter = document.querySelector('.counter')
 const modal = document.querySelector('.modal')
 const field = document.querySelector('.game-field')
 const fieldRect = field.getBoundingClientRect()
@@ -7,7 +8,7 @@ const CARROT_SIZE = 80
 
 const playIcon = document.querySelector('.play-icon')
 let started = false // 게임의 상태를 기억하는 변수
-let second = 10
+let second = 8
 let interval
 
 playBtn.addEventListener('click', () => {
@@ -22,18 +23,28 @@ playBtn.addEventListener('click', () => {
 })
 
 function startGame() {
+  showTimerScore()
   startTimer()
   onField()
 }
 
 function stopGame() {
   stopTimer()
+  removePlayBtn()
+}
+
+function showTimerScore() {
+  playIcon.setAttribute('class', 'fas fa-stop')
+  timer.classList.remove('state')
+  counter.classList.remove('state')
+}
+
+function removePlayBtn() {
+  playBtn.classList.add('state')
 }
 
 // 타이머
 function startTimer() {
-  playIcon.setAttribute('class', 'fas fa-stop')
-
   interval = setInterval(function () {
     timer.innerHTML = `${second}`
     second--
@@ -41,7 +52,6 @@ function startTimer() {
     if (second === -1) {
       clearInterval(interval)
       modal.classList.remove('hidden')
-      playBtn.remove()
     }
   }, 1000)
 }
@@ -49,11 +59,11 @@ function startTimer() {
 function stopTimer() {
   clearInterval(interval)
   modal.classList.remove('hidden')
-  playBtn.remove()
 }
 
 // 아이템 필드 위에 배치하기
 function onField() {
+  field.innerHTML = ''
   addItem('carrot', 6, 'img/carrot.png')
   addItem('bug', 6, 'img/bug.png')
 }
@@ -61,7 +71,8 @@ function onField() {
 function addItem(name, count, imgPath) {
   const x1 = 0 // 사각형의 시작점 (좌측 상단)
   const y1 = 0
-  const x2 = fieldRect.width - CARROT_SIZE // 사각형 우측 하단. 빼는 이유 - 아이템의 x,y 기준으로 포지션이 지정되기 때문에
+  const x2 = fieldRect.width - CARROT_SIZE // 사각형 우측 하단.
+  //빼는 이유 - 아이템의 x,y 기준으로 포지션이 지정되기 때문에
   const y2 = fieldRect.height - CARROT_SIZE
   for (let i = 0; i < count; i++) {
     const item = document.createElement('img')
